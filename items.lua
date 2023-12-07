2af62fbeb5e846effc30f7daa5b7e2f4997f9309
Etruzki <153145724+Etruzki@users.noreply.github.com>
2023-12-07 03:14:51 +0200

    Add files via upload

---
+++ items.lua
@@ -0,0 +1,48 @@
+return {
+PlaceObj('ModItemCharacterDef', {
+	Bio = T(729322060836, --[[ModItemCharacterDef Testaus Bio]] "Tuli testaamaan"),
+	FavoriteFood = {
+		"Porridge",
+	},
+	FavoriteRelaxation = {
+		"BenchPressing",
+		"PunchingPole",
+	},
+	FirstName = T(718195932272, --[[ModItemCharacterDef Testaus FirstName]] "Testaus"),
+	Gender = "Male",
+	Hair = "Hair_Greyson",
+	HairColor1 = 4278848010,
+	HairColor2 = 4279111182,
+	HairColor3 = 4279242511,
+	Head = "Head_Ken",
+	Headline = T(912622789384, --[[ModItemCharacterDef Testaus Headline]] "Testaaja"),
+	LastName = T(819046466842, --[[ModItemCharacterDef Testaus LastName]] "Testinen"),
+	NickName = T(454151430796, --[[ModItemCharacterDef Testaus NickName]] "Testi"),
+	PerceivedGender = "Male",
+	Portrait = "Character_small",
+	PortraitInventory = "UI/Portraits/Inventory/Ken_inventory",
+	PortraitLarge = "Character_large",
+	SkillInclinations = {
+		Combat = "Interested",
+	},
+	Skills = {
+		Combat = 6,
+		Construction = 2,
+		Cooking = 2,
+		Crafting = 2,
+		Farming = 2,
+		Healing = 2,
+		Intellectual = 2,
+		Physical = 4,
+	},
+	SkinColor = PlaceObj('ColorizationPropSet', {
+		'EditableColor1', RGBA(220, 198, 125, 255),
+		'EditableColor2', RGBA(216, 203, 155, 255),
+		'EditableColor3', RGBA(170, 157, 108, 255),
+	}),
+	Traits = {
+		"Bloodlust",
+	},
+	id = "Testaus",
+}),
+}
---
+++ metadata.lua
@@ -0,0 +1,14 @@
+return PlaceObj('ModDef', {
+	'title', "Testaus Mod",
+	'description', "Testaus Mod",
+	'image', "Preview",
+	'last_changes', "Testaus Mod",
+	'id', "DbPCPzm",
+	'author', "Etruzki",
+	'version', 11,
+	'lua_revision', 233360,
+	'saved_with_revision', 344982,
+	'saved', 1701887876,
+	'steam_id', "3106598021",
+	'TagCharacters', true,
+})
